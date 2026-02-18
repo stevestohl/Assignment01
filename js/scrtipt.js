3d3aa17c47d5918ebbec672af902b92b844d6b37
Stevestohl <stevegerring@gmail.com>
2026-02-18 08:30:06 -0800

    //Final commit

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -1,1 +1,135 @@
-document.write('hello')
+// document.write('hello')
+
+//Step 1
+// // let someMonth
+// // function theMonth()
+// // let currentMonth
+// // let summerMonth
+// // let myLibraryFunction
+
+//Step 2 
+// numeric literal expression: 8
+// string literal expression: 'This is a string'
+// Boolean literal expression false
+// null literal expression: null
+
+//Step 3
+// Give me two examples of complex / variable expressions.
+// fahrenheit = (9/5)*c+32
+// total = price * quantity
+
+//Step 4
+// let strFirstName, 
+//     strLastName, 
+//     strAddress,
+//     strCity, 
+//     strState, 
+//     strZipCode, 
+//     numYourAge, 
+//     strReferralSource, 
+//     boolMayWeContactYou;
+
+//Step 5
+
+//multiline declaration
+// let strFirstName = 'Steve';
+// strFirstName = 'Steve';
+
+//single line declaration
+// let strlLastName = 'Gerring';
+
+//multi-line declaration with 3 variables
+// let boolMayWeContactYou, strCity, numZipCode;
+// boolMayWeContactYou = true;
+// strCity = "San Diego";
+// numZipCode = 92101;
+
+
+//Step 6
+// let strGroceryStore = 'Ralphs';
+// console.log("Steven went to " + strGroceryStore + " " + 3 + " times today.");
+
+//Step 7
+// let boolMayWeContactYou = true;
+// let strPreference = "The contact preference you selected is " + boolMayWeContactYou;
+// console.log(strPreference);
+
+// let numYourAge = 30;
+// let boolHasYourBirthDayHappened =numYourAge + false;
+// console.log(boolHasYourBirthDayHappened);
+
+//Step 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString);
+
+
+//Step 9
+// let nothing = null;
+// console.log(nothing);
+
+// let notAssigned;
+// console.log(notAssigned);
+
+//Step 10
+// typeof "This is a string";
+// typeof 8;
+// typeof true;
+// typeof null;
+// let notAssigned;
+// typeof notAssigned;
+
+//Step 11
+// let myName  = 'Steven Gerring';
+// alert('Hello ' + myName + ', welcome to the Javascript class!');
+
+//Step 12
+// let myName2 = 'Steve';
+// alert('Hello ' + myName2 + ', welcome to the Javascript class!');
+
+//Step 13
+// let myName2 = 'Steve';
+// let course = 'JavaScript';
+// alert('Hello ' + myName2 + ', welcome to the ' + course + ' class!');
+
+//Step 14
+// let myName2 = 'Steve';
+// let course = 'JavaScript';
+// alert('Hello ' + myName2 + '.\nWelcome to the ' + course + ' class!');
+
+//Step 15
+// let myName2 = prompt('Enter Name: ');
+// let course = 'JavaScript';
+// alert('Hello ' + myName2 + '.\nWelcome to the ' + course + ' class!');
+
+//Step 16
+// let myName2 = prompt('Enter Name: ');
+// let course = prompt('Enter Course Name');
+// alert('Hello ' + myName2 + '.\nWelcome to the ' + course + ' class!');
+
+//Step 17
+// let x = 10;
+// let y = 20;
+// console.log(x+y);
+
+//Step 18
+// let x = 20;
+// console.log(x + 20);
+
+//Step 19
+// let x = 20;
+// console.log(x*5);
+
+//Step 20
+// let x = 20 % 3;
+// console.log(x/1);
+
+//Step 21
+//Shows if my age is between 30 and 45
+// let myAge = 40;
+// let inAgeRange = (myAge > 30) && (myAge < 45);
+// console.log(inAgeRange);
+
+//Step 22
+// let myAge = 40;
+// let inAgeRange = (myAge >= 55) || (myAge == 45);
+// console.log(inAgeRange);
